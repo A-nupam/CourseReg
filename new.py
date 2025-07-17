b7f8ed06a3b6447cb475de0200af7b3f2925ee66
A-nupam <anupammahato781@gmail.com>
2025-07-17 08:47:36 +0530

    add buzzer

--- new.py
+++ new.py
@@ -2,6 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import urllib3
+import pygame;
+pygame.mixer.init()
+def play_buzzer():
+    pygame.mixer.music.load("buzzer.mp3")
+    pygame.mixer.music.play()
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -10,20 +15,21 @@ REGISTRATION_BASE = "https://reg.exam.dtu.ac.in/student/courseRegister/6497168af
 REFRESH_INTERVAL = 5  # seconds
 
 TARGET_COURSES = {
+    "CYBER LAWS":"686e5513d00bc35c8c12b7ef",
+    "INDIAN ECONOMY": "686e5513d00bc35c8c12b73b",
+    "BASIC COMMUNICATION SKILLS":"686e5513d00bc35c8c12b73f",
+    "MARKETING MANAGEMENT":"686e5513d00bc35c8c12b747",
     "BASIC ECONOMETRICS": "686e5513d00bc35c8c12b7f3",
     "LOGISTICS MANAGEMENT": "686e5513d00bc35c8c12b7f7",
     "MACROECONOMICS": "686e5513d00bc35c8c12b813",
     "CREATIVE WRITING SKILLS": "686e5513d00bc35c8c12b817",
     "MARKETING RESEARCH": "686e5513d00bc35c8c12b81f",
     "TOTAL QUALITY MANAGEMENT": "686e5513d00bc35c8c12b823",
-    "MALWARE ANALYSIS": "686e5513d00bc35c8c12b86f",
-    "SECURE CODING": "686e5513d00bc35c8c12b873",
-    "MULTI MODAL DATA PROCESSING": "686e5513d00bc35c8c12b893",
 }
 
 COOKIES = {
-    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDk3MTY4YWY2MDhkOTU3ZWMzYjU3NzgiLCJzc2lkIjo2NjMzLCJpYXQiOjE3NTI1NDc4MDd9.HIocmpEwm3uGn97gSiLIybbSngzIUQzgUwXxh1DzaYk",  # <- Replace this with your token
-    "connect.sid": "s%3A-nKZRRcs8shgv8vNRHjdWeSmEiahC4Ad.QZLD7aWcg67gynUKdEfXmNslaUKJypTIFpQnJeTP5Os",  # <- Replace this with your session ID
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDk3MTY4YWY2MDhkOTU3ZWMzYjU3NzgiLCJzc2lkIjozMDc0LCJpYXQiOjE3NTI2MzMxODV9.IRYQpJoU3OvMqwPp5XxhPVc0XwbjhrH6_nN3mB8eoKo",
+    "connect.sid": "s%3AH5JQBo0DaAYDq0WS9lMrjhv9BSUicUXS.CbwWLxzvLH4A0zheJG97%2FE%2B1vA4j9R6wS9FwaUfBbgc"
 }
 
 HEADERS = {
@@ -46,7 +52,12 @@ def check_and_register():
                 print(f"🎯 Seat Available: {course_name} → {seat_text} → Registering now...")
                 reg_url = REGISTRATION_BASE + TARGET_COURSES[course_name]
                 reg = requests.post(reg_url, headers=HEADERS, cookies=COOKIES, verify=False)
-                print("✅ Registered!" if reg.status_code == 200 else f"⚠️ Failed! Status: {reg.status_code}")
+                # print("✅ Registered!" if reg.status_code == 200 else f"⚠️ Failed! Status: {reg.status_code}")
+                if reg.status_code ==200:
+                    print("Registered!! Yay")
+                    play_buzzer()
+                else:
+                    print(f"Failed! Status:  {reg.status_code}")
                 return True
     return False
 
